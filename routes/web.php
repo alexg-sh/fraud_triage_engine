7843a018149215d7e9e2a17f95c175c7c13edc8d
Alex Grigoras <me@alexg.sh>
2026-04-20 08:09:13 +0100

    FEAT: add a queue monitor route

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\OrderDashboardController;
+use App\Http\Controllers\QueueMonitorController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/orders');
 
 Route::get('/orders', OrderDashboardController::class)
     ->name('orders.index');
+
+Route::get('/horizon', QueueMonitorController::class)
+    ->name('queue.monitor');
 
 Route::post('/orders', [OrderController::class, 'store'])
     ->name('orders.store');
