820cbec9ac002d3bb1e469e244cf7ce3cd221c45
Alex Grigoras <me@alexg.sh>
2026-04-20 08:09:13 +0100

    STYLE: enable the dark app shell by default

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,7 +8,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.tsx'])
         @inertiaHead
     </head>
-    <body class="theme min-h-screen bg-background font-sans text-foreground antialiased">
+    <body class="theme dark min-h-screen bg-background font-sans text-foreground antialiased">
         @inertia
     </body>
 </html>
